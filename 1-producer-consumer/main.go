759fd3ed6a72a284a2a1bf2208ee0ac2cc59100b
sam46 <abdlwahdsa@gmail.com>
2021-12-24 00:20:21 -0500

    solved 1

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -13,19 +13,24 @@ import (
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
-	for {
-		tweet, err := stream.Next()
-		if err == ErrEOF {
-			return tweets
-		}
+func producer(stream Stream) <-chan Tweet {
+	tweets := make(chan Tweet, 10)
+	go func(tweets chan<- Tweet) {
+		defer close(tweets)
+		for {
+			tweet, err := stream.Next()
+			if err == ErrEOF {
+				return
+			}
 
-		tweets = append(tweets, tweet)
-	}
+			tweets <- *tweet
+		}
+	}(tweets)
+	return tweets
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
+func consumer(tweets <-chan Tweet) {
+	for t := range tweets {
 		if t.IsTalkingAboutGo() {
 			fmt.Println(t.Username, "\ttweets about golang")
 		} else {
